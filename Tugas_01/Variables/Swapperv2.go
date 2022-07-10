71e400f67bc49a0e7e8cf3460e10d872fa44286c
reski-id <pesan.reski@gmail.com>
2022-07-10 17:39:46 +0700

    swapper2

--- Tugas_01/Variables/Swapperv2.go
+++ Tugas_01/Variables/Swapperv2.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 // ---------------------------------------------------------
 // EXERCISE: Swapper #2
 //
@@ -14,8 +16,10 @@ package main
 func main() {
 	// UNCOMMENT THE CODE BELOW:
 
-	// red, blue := "red", "blue"
-	// ?
+	red, blue := "red", "blue"
+	red, blue = blue, red
+
+	fmt.Println(red, blue)
 }
 
 //https://github.com/inancgumus/learngo/tree/master/06-variables/04-assignment/exercises/09-swapper-2
