730dcb55659794946c66307dc8ad60a0bf6d3482
reski-id <pesan.reski@gmail.com>
2022-07-14 00:13:12 +0700

    add link github

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 //https://github.com/inancgumus/learngo/tree/master/07-printf
 //https://www.udemy.com/course/learn-go-the-complete-bootcamp-course-golang/learn/lecture/11763462#overview
+//https://github.com/reski-id/1000ProjectGolang
 
 package main
 
